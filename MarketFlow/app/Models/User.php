50ed6f841f91eb3adbcf6c5f649127dba9dd23be
Uriel Saavedra <saavedraangel514@gmail.com>
2026-05-06 18:36:22 -0600

    feat:Al parecer el CRUD de las direcciones ya esta listo

--- MarketFlow/app/Models/User.php
+++ MarketFlow/app/Models/User.php
@@ -66,4 +66,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function direcciones()
+    {
+        return $this->hasMany(Direccion::class, 'id_user', 'id');
+    }
 }
